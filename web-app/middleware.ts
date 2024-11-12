f7f8f2945649bdd0d5752d35148dcfbb530a955b
Jaweki <werukioni@gmail.com>
2024-11-12 21:14:50 +0300

    added documentation of the changes made; for redux and graphql

--- web-app/middleware.ts
+++ web-app/middleware.ts
@@ -1,19 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // here public and admin routes are defined anything else is a protected route
-const isPublicRoute = createRouteMatcher(['/'])
-const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+const isPublicRoute = createRouteMatcher(["/"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+const isReduxPage = createRouteMatcher(["/redux-graphql(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID // here I used clerk Id but it could be changed to email 
-  if (isAdminRoute(req) && !isAdminUser) {
-    return NextResponse.redirect(new URL( '/admin', req.url))
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID; // here I used clerk Id but it could be changed to email
+  if (isAdminRoute(req) && isAdminUser) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  } else if (isReduxPage(req) && isAdminUser) {
+    return NextResponse.redirect(new URL("/redux-graphql", req.url));
   }
-  if (!isPublicRoute(req)) auth().protect()
-})
+  if (!isPublicRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
-
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
