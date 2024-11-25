a110592058c3a3df7fe20f15e781259f2cd971ce
Jaweki <werukioni@gmail.com>
2024-11-25 21:25:38 +0300

    debug; forced map-view page to render as dynamic page (CSR)

--- web-app/app/admin/map-view/page.tsx
+++ web-app/app/admin/map-view/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import MapRenderer from "./ui/MapRenderer";
 
-function MapView() {
+export const dynamic = "force-dynamic"; // For Next.js App Router
+
+const MapView = () => {
   return <MapRenderer />;
-}
+};
 
 export default MapView;
